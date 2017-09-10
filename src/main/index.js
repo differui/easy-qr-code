132cdfd5a676881a75d10d3f175da256f306b93f
differui <differui@gmail.com>
2017-09-10 16:51:50 +0800

    :package: customize application menu

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,6 @@
 import { app, BrowserWindow, ipcMain, Menu, shell } from 'electron' // eslint-disable-line
 import { image } from 'qr-image';
 import { join as joinPath } from 'path';
-import defaultMenu from 'electron-default-menu';
 
 /**
  * Set `__static` path to static files in production
@@ -31,8 +30,142 @@ function createWindow() {
   });
 }
 
+function createMenuTemplate(app, shell) {
+  const menuTemplate = [
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          role: 'undo',
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          role: 'redo',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          role: 'cut',
+        },
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          role: 'copy',
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          role: 'paste',
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          role: 'selectall',
+        },
+      ],
+    },
+    {
+      label: 'Window',
+      role: 'window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'CmdOrCtrl+M',
+          role: 'minimize',
+        },
+        {
+          label: 'Close',
+          accelerator: 'CmdOrCtrl+W',
+          role: 'close',
+        },
+      ],
+    },
+    {
+      label: 'Help',
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click() {
+            shell.openExternal('http://electron.atom.io');
+          },
+        },
+      ],
+    },
+  ];
+
+  if (process.platform === 'darwin') {
+    const name = app.getName();
+
+    menuTemplate.unshift({
+      label: name,
+      submenu: [
+        {
+          label: `About ${name}`,
+          role: 'about',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Services',
+          role: 'services',
+          submenu: [],
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: `Hide ${name}`,
+          accelerator: 'Command+H',
+          role: 'hide',
+        },
+        {
+          label: 'Hide Others',
+          accelerator: 'Command+Shift+H',
+          role: 'hideothers',
+        },
+        {
+          label: 'Show All',
+          role: 'unhide',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click() {
+            app.quit();
+          },
+        },
+      ],
+    });
+
+    const windowMenu = menuTemplate.find(m => m.role === 'window');
+
+    if (windowMenu) {
+      windowMenu.submenu.push({
+        type: 'separator',
+      });
+      windowMenu.submenu.push({
+        label: 'Bring All to Front',
+        role: 'front',
+      });
+    }
+  }
+
+  return menuTemplate;
+}
+
 function createMenu() {
-  const menu = defaultMenu(app, shell);
+  const menu = createMenuTemplate(app, shell);
 
   Menu.setApplicationMenu(Menu.buildFromTemplate(menu));
 }
