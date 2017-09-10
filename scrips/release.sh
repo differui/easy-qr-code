3cfde2cec28c1e33a5a5baa618799920d44edecc
differui <differui@gmail.com>
2017-09-10 18:18:53 +0800

    :construction_worker: release script

---
+++ scrips/release.sh
@@ -0,0 +1,4 @@
+tar -zcvf ./build/Easy\ QR\ Code-darwin-x64.zip ./build/Easy\ QR\ Code-darwin-x64
+tar -zcvf ./build/Easy\ QR\ Code-linux-x64.zip ./build/Easy\ QR\ Code-linux-x64
+tar -zcvf ./build/Easy\ QR\ Code-mas-x64.zip ./build/Easy\ QR\ Code-mas-x64
+tar -zcvf ./build/Easy\ QR\ Code-win32-x64.zip ./build/Easy\ QR\ Code-win32-x64
